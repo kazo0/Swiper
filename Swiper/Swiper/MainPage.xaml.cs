2781e53741cb537703cba176b5fa4593d1836807
Steve Bilogan <steve.bilogan@gmail.com>
2020-07-19 17:09:01 -0400

    Finish app, adding animations

--- Swiper/Swiper/MainPage.xaml.cs
+++ Swiper/Swiper/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Swiper.Controls;
 using Xamarin.Forms;
 
 namespace Swiper
@@ -13,9 +14,47 @@ namespace Swiper
 	[DesignTimeVisible(false)]
 	public partial class MainPage : ContentPage
 	{
+		private int _likeCount;
+		private int _denyCount;
+
 		public MainPage()
 		{
 			InitializeComponent();
+			AddInitialPhotos();
+		}
+
+		private void Handle_OnLike(object sender, EventArgs e)
+		{
+			_likeCount++;
+			InsertPhoto();
+			UpdateGui();
+		}
+
+		private void Handle_OnDeny(object sender, EventArgs e)
+		{
+			_denyCount++;
+			InsertPhoto();
+			UpdateGui();
+		}
+
+		private void AddInitialPhotos()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				InsertPhoto();
+			}
+		}
+		private void InsertPhoto()
+		{
+			var photo = new SwiperControl();
+			photo.OnDeny += Handle_OnDeny;
+			photo.OnLike += Handle_OnLike;
+			this.MainGrid.Children.Insert(0, photo);
+		}
+		private void UpdateGui()
+		{
+			likeLabel.Text = _likeCount.ToString();
+			denyLabel.Text = _denyCount.ToString();
 		}
 	}
 }
